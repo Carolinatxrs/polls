a465ac1185fa4754d62f0bf96de33b9c666f43e6
Carolina Teixeira <maria.silva@creathus.org.br>
2024-02-12 19:33:02 -0400

    feat: added Redis for caches and organized the ranking of the most voted options

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,7 @@ import { z } from "zod"
 import { randomUUID } from 'node:crypto';
 import { prisma } from "../../lib/prisma"
 import { FastifyInstance } from "fastify"
+import { redis } from "../../lib/redis";
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post('/polls/:pollId/votes', async (req, reply) => {
@@ -34,6 +35,8 @@ export async function voteOnPoll(app: FastifyInstance) {
             id: userPreviousVoteOnPoll.id
           }
         })
+
+        await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
       } else if (userPreviousVoteOnPoll) {
         return reply.status(400).send({
           message: 'You already voted on this poll.'
@@ -59,6 +62,8 @@ export async function voteOnPoll(app: FastifyInstance) {
         pollOptionId
       }
     })
+
+    await redis.zincrby(pollId, 1, pollOptionId)
     
     return reply.status(201).send()
   })
